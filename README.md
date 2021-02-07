ee20788a2d12311817b1a4d60065adf2c239149b
Jesse Jaggars <jhjaggars@gmail.com>
2021-02-07 15:56:57 -0500

    initial commit
    Signed-off-by: Jesse Jaggars <jhjaggars@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+Space Trader
+============
+
+Space Trader is a resource trading and arbitrage game, inspired by dope wars,
+merchant of the skies and others.
+
+In this game, you play the commander of a trading vessel (for a wide definition
+of trading vessel) with the goal of amassing as many credits as possible, so
+that you can get out of the rat race and retire on a quiet backwater planet
+with your collection of banzai trees. 
+
+In the real world, as players connect to the server, their public ip address
+determines which location that they are playing at.  Each location can host
+several points of interest, such as wandering merchants who deal in legitimate
+wares that you may trade with, local constabulary who may wish to detain you
+for your supposed misdeeds, and even vicious reavers who offer nothing but
+misery and misfortune.
+
+Buy, barter, sell or steal in order to amass resources, and ultimately currency
+so that you can finally realize your dreams of taking it easy for as long as
+you damn well please, but beware it may be more challenging that it seems.
---
+++ src/index.js
@@ -0,0 +1,66 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello Project!');
+});
+
+
+let planets = {
+    1: {
+        id: 1,
+        name: 'Terra',
+        description: 'A temperate planet with moderate amounts of resources. Heavily populated with late-stage civilizations.',
+    },
+};
+
+
+let merchants = {
+    0: {
+        id: 0,
+        name: 'Simple Stan\'s Mercantile',
+        description: 'Trades in useful refined, yet inexpensive materials.',
+        inventory: [
+            {
+                id: 'item:battery',
+                name: 'battery',
+                price: 120,
+                quantity: 2480,
+            },
+        ],
+    }
+};
+
+
+app.get('/survey', (req, res) => {
+    let d = {
+        planet: planets[1],
+        help: `GET /survey returns basic information about where you are, including which planetary body you may be orbiting`,
+    };
+    return res.send(d);
+});
+
+app.get('/dock', (req, res) => {
+    return res.send(
+        `GET /dock lands your vessel and gets basic information about this location.`
+    );
+});
+
+app.get('/merchants/:id', (req, res) => {
+    return res.send(merchants[req.params.id]);
+});
+
+app.post('/merchants/:id', (req, res) => {
+    return res.send(req.body);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Express app listening on port ${process.env.PORT}.`);
+});
